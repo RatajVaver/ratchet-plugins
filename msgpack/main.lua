c118a8d7cfe34da6c0b76da51a0ef541438ac453
Rataj <rataj@leosight.cz>
2025-09-15 04:25:33 +0200

    luarocks (msgpack + inspect) showcase

---
+++ msgpack/main.lua
@@ -0,0 +1,27 @@
+-- This example showcases the compatibility with LuaRocks.
+-- Specifically MessagePack and inspect modules are shown here.
+-- They need to be installed for this example to work.
+
+-- You might need to specify package path like this, or set 'packages' in Ratchet config.
+-- package.path = package.path .. ";C:/Users/USERNAME/AppData/Roaming/luarocks/share/lua/5.4/?.lua"
+
+local mp = require 'MessagePack'
+local inspect = require 'inspect'
+
+local data = {
+    msg = "Hello world",
+    pi = math.pi
+}
+
+local packed = mp.pack(data)
+
+local file = io.open("data/test.msgpack", "w")
+file:write(packed)
+file:close()
+
+local file = io.open("data/test.msgpack", "r")
+local packed = file:read()
+file:close()
+
+local unpacked = mp.unpack(packed)
+printInfo("Data:\n", inspect(unpacked))
